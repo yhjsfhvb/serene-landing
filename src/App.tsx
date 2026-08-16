a45f55cd1d0ec0107eabeb9f0e04d17fe74f3fae
yhjsfhvb <1377514860@qq.com>
2026-08-16 20:41:15 +0800

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero'
+import QuoteSection from './components/QuoteSection'
+
+export default function App() {
+  return (
+    <div className="bg-[#0a0608]">
+      <Hero />
+      <QuoteSection />
+    </div>
+  )
+}
